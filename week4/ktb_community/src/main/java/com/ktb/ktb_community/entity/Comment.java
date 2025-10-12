8480d2d87f4526c957bf7f64e4281ee2e79a794d
Hyeseong-Myeong <a01025819869@gmail.com>
2025-10-12 22:35:56 +0900

    ktb_community_project init(#4)

--- week4/ktb_community/src/main/java/com/ktb/ktb_community/entity/Comment.java
+++ week4/ktb_community/src/main/java/com/ktb/ktb_community/entity/Comment.java
@@ -1,4 +1,38 @@
 package com.ktb.ktb_community.entity;
 
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    private String comment;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public Comment(String comment, User user, Post post) {
+        this.comment = comment;
+        this.user = user;
+        this.post = post;
+    }
+
+
 }
