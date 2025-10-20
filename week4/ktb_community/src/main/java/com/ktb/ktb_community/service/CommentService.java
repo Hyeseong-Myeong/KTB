19e76da8267ff0112d5bfd15b67fc314851e0b34
Hyeseong-Myeong <a01025819869@gmail.com>
2025-10-20 13:16:07 +0900

    feat: comment service

--- week4/ktb_community/src/main/java/com/ktb/ktb_community/service/CommentService.java
+++ week4/ktb_community/src/main/java/com/ktb/ktb_community/service/CommentService.java
@@ -1,4 +1,14 @@
 package com.ktb.ktb_community.service;
 
+import com.ktb.ktb_community.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+
 }
